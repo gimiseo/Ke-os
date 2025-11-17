fea11e1129c7bab3aa818ed2b7cf08479ea8570d
namhyunseo <namhyunseo0224@gmail.com>
2025-11-17 19:53:39 +0900

    1차 미팅 전 커밋 >.-

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -41,6 +41,7 @@ process_init (void) {
 tid_t
 process_create_initd (const char *file_name) {
 	char *fn_copy;
+	char fn_copy2[32];
 	tid_t tid;
 
 	/* Make a copy of FILE_NAME.
@@ -50,8 +51,12 @@ process_create_initd (const char *file_name) {
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 
+	/* 우선순위 안나오는 현상 제거 */
+	strlcpy (fn_copy2, file_name, sizeof(fn_copy2));
+	char *save;
+	char *prog_name = strtok_r(fn_copy2, " ", &save);
 	/* Create a new thread to execute FILE_NAME. */
-	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
+	tid = thread_create (prog_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
 		palloc_free_page (fn_copy);
 	return tid;
